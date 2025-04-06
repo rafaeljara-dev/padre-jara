fb05a1a347eb4edfd095395cb3826210d98176b5
rafaeljara-dev <rafaeljara73@gmail.com>
2025-04-05 19:08:06 -0500

    hotfix on X

--- components/app-sidebar.tsx
+++ components/app-sidebar.tsx
@@ -3,7 +3,7 @@
 import { useState, useEffect } from "react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-import { LayoutDashboard, FileText, Menu, X } from "lucide-react";
+import { LayoutDashboard, FileText, Menu } from "lucide-react";
 
 import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
